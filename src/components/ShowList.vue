6a98b2fa179fd2ac690fd0cb4d509c6794f7e72f
viajiefan <viajiefan@gmail.com>
2023-09-07 22:16:27 +0900

    feat change underline algo, change profile

--- src/components/ShowList.vue
+++ src/components/ShowList.vue
@@ -6,13 +6,13 @@
                 <a id="icon_link" v-if="pub.pdf" v-bind:href="pub.pdf" target="_blank" rel="noopener noreferrer"><i class="fa-regular fa-file-pdf"></i></a>
                 <a id="icon_link" v-if="pub.link" v-bind:href="pub.link" target="_blank" rel="noopener noreferrer"><i class="fa-solid fa-link"></i></a>
                 <br>
-                
+
                 <!-- Print Author names -->
                 <!-- <span>Authors: {{ pub.author.join(', ') }}</span> <br> -->
                 <span>Authors: </span>
                 <template v-for="(author_el, index) in pub.author" :key="index">
-                    <!-- if myname, underline -->
-                    <span v-if="author_el === top.JaName || author_el === top.EngName" id="myname"> {{author_el}} </span>
+                    <!-- if myname(compair in no space string), underline -->
+                    <span v-if="author_el.replace(/\s+/g, '') === top.JaName.replace(/\s+/g,'') || author_el.replace(/\s+/g,'') === top.EngName.replace(/\s+/g,'')" id="myname"> {{author_el}} </span>
                     <span v-else> {{author_el}} </span>
                     <span v-if="index != pub.author.length-1">, </span>
                 </template><br>
