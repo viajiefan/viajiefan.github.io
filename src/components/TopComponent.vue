6714dfbe3fc0404457ba3691cb715036ebe53e41
viajiefan <viajiefan@gmail.com>
2023-12-01 14:53:23 +0900

    update mail address

--- src/components/TopComponent.vue
+++ src/components/TopComponent.vue
@@ -2,7 +2,8 @@
     <div class="top">
         <h1> {{descObj.Top.JaName}} ({{descObj.Top.EngName}})</h1>
         <p> ({{descObj.Top.Yomi}}) </p>
-        <p> <i class="fa-regular fa-envelope"></i> {{descObj.Top.Mail}}</p>
+        <p> <i class="fa-regular fa-envelope"></i> {{descObj.Top.Mail1}}</p>
+        <p> <i class="fa-regular fa-envelope"></i> {{descObj.Top.Mail2}}</p>
         <p> {{descObj.Top.Explain}} </p>        
     </div>
 </template>
