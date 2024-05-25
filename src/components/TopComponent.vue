92af326f14c1e61badd1d352b503a9d9f142aba0
viajiefan <viajiefan@gmail.com>
2024-05-26 00:04:09 +0900

    fix open link in new tab

--- src/components/TopComponent.vue
+++ src/components/TopComponent.vue
@@ -5,7 +5,7 @@
         <p> <i class="fa-regular fa-envelope"></i> {{descObj.Top.Mail1}}</p>
         <p> <i class="fa-regular fa-envelope"></i> {{descObj.Top.Mail2}}</p>
         <p> {{descObj.Top.Explain}} </p>  
-        <p> <a v-bind:href="descObj.Top.GitHub"><i class="fa-brands fa-github"></i> GitHub</a></p>
+        <p> <a v-bind:href="descObj.Top.GitHub" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-github"></i> GitHub</a></p>
     </div>
 </template>
 
