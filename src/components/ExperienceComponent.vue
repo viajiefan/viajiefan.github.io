6c8a70b7bbf51597335bf118fcb11e810b220184
viajiefan <viajiefan@gmail.com>
2024-08-12 16:54:19 +0900

    add paper, project info

--- src/components/ExperienceComponent.vue
+++ src/components/ExperienceComponent.vue
@@ -2,14 +2,17 @@
     <h2>Experience</h2>
 
     <div class="top">
-        <li>2023.05 - present 成蹊大学 理工学部 特別共同研究員 (知的インタフェース研究室)</li>
+        <li>2023.05 - present 成蹊大学 理工学部 特別共同研究員 (知的インタフェース研究室) (Seikei University, Intelligent User Interface Lab.)
+        <p class="desc"><a href="http://iui.ci.seikei.ac.jp/" target="_blank" rel="noopener noreferrer">HP</a></p>
+        </li>
         
     </div>
 
     <h2>Activities</h2>
     <div class="top">
-        <li>2024.03 - present GENIAC 松尾研 LLM開発プロジェクト
-        <p class="desc">Phase1 Team Zooメンバー<a href="https://weblab.t.u-tokyo.ac.jp/geniac_llm/teams" target="_blank" rel="noopener noreferrer">(URL)</a></p>
+        <li>2024.08 - present 松尾研 大規模データ収集プロジェクト (Matsuo Lab., Large-scale data collection project for LLM development)</li>
+        <li>2024.03 - 2024.08 GENIAC 松尾研 LLM開発プロジェクト (GENIAC, Matsuo Lab., LLM development project)
+        <p class="desc">Phase1 Team Zooメンバー<a href="https://weblab.t.u-tokyo.ac.jp/geniac_llm/teams" target="_blank" rel="noopener noreferrer">(URL)</a> & Phase2メンバー</p>
         </li>
     </div>
 
